3e5e57ba0f7fb1d8ffe893ee8074a00b1d64c157
tanjim840 <tanjim.techforing@gmail.com>
2022-10-14 18:52:37 +0600

    added gunicorn socket & service file

--- gunicorn.sh
+++ gunicorn.sh
@@ -2,4 +2,28 @@
 
 source /env/bin/activate
 
+# Doesn't require to get inside the "app" folder, since the current dir is holding the "manage.py" file.
+cd /var/lib/jenkins/workspace/Django CI-CD/
+
+python3 manage.py makemigrations
+python3 manage.py migrate
+
+echo "Django migrations have been done!"
+
+cd ../
+
+sudo cp -rf gunicorn.scoket /etc/systemd/system/
+sudo cp -rf gunicorn.service /etc/systemd/system/
+
+echo "User: $USER"
+echo "Current Directory: $PWD"
+
+sudo systemctl daemon-reload
+sudo systemctl start gunicorn
+sudo systemctl enable gunicorn
+
+echo "Gunicorn has been started"
+
+sudo systemctl status gunicorn
+sudo systemctl restart gunicorn
 
