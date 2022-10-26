3523a37ecf931683d3741526783fe520b1add3f1
ashraf840 <shift2unleashed2011@gmail.com>
2022-10-26 22:42:22 +0600

    Modify gunicorn_django_cicd to gunicorn

--- gunicorn.sh
+++ gunicorn.sh
@@ -14,21 +14,19 @@ echo "Django migrations have been done while executing gunicorn.sh!"
 
 echo "Present Directory: $PWD"
 
-sudo cp -rf gunicorn_django_cicd.socket /etc/systemd/system/
-sudo cp -rf gunicorn_django_cicd.service /etc/systemd/system/
+sudo cp -rf gunicorn.socket /etc/systemd/system/
+sudo cp -rf gunicorn.service /etc/systemd/system/
 
-echo "Successfully copied the 'gunicorn_django_cicd.socket' & 'gunicorn_django_cicd.service' files into '/etc/systemd/system/' dir"
+echo "Successfully copied the 'gunicorn.socket' & 'gunicorn.service' files into '/etc/systemd/system/' dir"
 echo "User: $USER"
 echo "Current Directory: $PWD"
 
 sudo systemctl daemon-reload
-sudo systemctl start gunicorn_django_cicd.socket
-sudo systemctl enable gunicorn_django_cicd.socket
-sudo systemctl start gunicorn_django_cicd.service
-sudo systemctl enable gunicorn_django_cicd.service
+sudo systemctl start gunicorn
+sudo systemctl enable gunicorn
 
 echo "Gunicorn (Django CI CD - App1) is about to be started!"
 
-sudo systemctl status gunicorn_django_cicd
-sudo systemctl restart gunicorn_django_cicd
+sudo systemctl status gunicorn
+sudo systemctl restart gunicorn
 
