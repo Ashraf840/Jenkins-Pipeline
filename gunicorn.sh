a5cf5566d045022819d09ab2ce1fc13925cbc108
tanjim840 <tanjim.techforing@gmail.com>
2022-10-26 19:07:30 +0600

    Place the app into a subdomain: app1.teachatty.xyz

--- gunicorn.sh
+++ gunicorn.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "running gunicorn.sh file"
+echo "running gunicorn.sh file (App1)"
 echo "$PWD"
 source env/bin/activate
 
@@ -10,23 +10,25 @@ cd /var/lib/jenkins/workspace/django_cicd/
 python3 manage.py makemigrations
 python3 manage.py migrate
 
-echo "Django migrations have been done by gunicorn.sh!"
+echo "Django migrations have been done while executing gunicorn.sh!"
 
 echo "Present Directory: $PWD"
 
-sudo cp -rf gunicorn.socket /etc/systemd/system/
-sudo cp -rf gunicorn.service /etc/systemd/system/
+sudo cp -rf gunicorn_django_cicd.socket /etc/systemd/system/
+sudo cp -rf gunicorn_django_cicd.service /etc/systemd/system/
 
-echo "Successfully copied the 'gunicorn.socket' & 'gunicorn.service' files into '/etc/systemd/system/' dir"
+echo "Successfully copied the 'gunicorn_django_cicd.socket' & 'gunicorn_django_cicd.service' files into '/etc/systemd/system/' dir"
 echo "User: $USER"
 echo "Current Directory: $PWD"
 
 sudo systemctl daemon-reload
-sudo systemctl start gunicorn
-sudo systemctl enable gunicorn
+sudo systemctl start gunicorn_django_cicd.socket
+sudo systemctl enable gunicorn_django_cicd.socket
+sudo systemctl start gunicorn_django_cicd.service
+sudo systemctl enable gunicorn_django_cicd.service
 
-echo "Gunicorn is about to be started!"
+echo "Gunicorn (Django CI CD - App1) is about to be started!"
 
-sudo systemctl status gunicorn
-sudo systemctl restart gunicorn
+sudo systemctl status gunicorn_django_cicd
+sudo systemctl restart gunicorn_django_cicd
 
