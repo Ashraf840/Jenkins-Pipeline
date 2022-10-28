26d44957d9f7490ae8bcbcd94793e96d01d0c8d5
ashraf840 <shift2unleashed2011@gmail.com>
2022-10-29 01:07:59 +0600

    Renamed the supervisor's conf file

--- supervisor.sh
+++ supervisor.sh
@@ -6,9 +6,9 @@ echo "Present Directory: $PWD"
 
 sudo apt install supervisor
 
-# Copy 'multi_apps_1.supervisor.conf' file into '/etc/supervisor/conf.d/' dir, thus supervisor can run the
-# shell-script to start this django-apps gunicorn particularly.
-sudo cp -rf multi_apps_1.supervisor.conf /etc/supervisor/conf.d/multi_apps_1.supervisor.conf
+# Copy 'multi_apps_1_supervisor.conf' file into '/etc/supervisor/conf.d/' dir, thus supervisor can run the
+# shell-script to start this django-app's gunicorn particularly.
+sudo cp -rf multi_apps_1_supervisor.conf /etc/supervisor/conf.d/multi_apps_1_supervisor.conf
 
 # Check if supervisor.log exist, otherwise create the file. This file is used inside the 'multi_apps_1.supervisor.conf' file
 if [ -e logs/gunicorn_supervisor.log ]
@@ -20,5 +20,5 @@ else
 fi
 
 
-sudo supervisorctl start multi_dj_app1.supervisor
-sudo supervisorctl status multi_dj_app1.supervisor
+sudo supervisorctl start multi_apps_1_supervisor
+sudo supervisorctl status multi_apps_1_supervisor
