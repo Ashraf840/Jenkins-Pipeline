48349309aa39adbfdfb5b56037940f491354b25c
ashraf840 <shift2unleashed2011@gmail.com>
2022-10-29 16:39:01 +0600

    Update the 'nginx.sh' file

--- nginx.sh
+++ nginx.sh
@@ -3,6 +3,10 @@
 echo "running nginx.sh file"
 echo "Present Directory: $PWD"
 
+NGINX_CONF_FILE=multi_apps_1_nginx.conf
+NGINX_SITES_AVAILABLE_DIR=/etc/nginx/sites-available
+NGINX_SITES_ENABLED_DIR=/etc/nginx/sites-enabled
+
 # Check if nginx_access.log & nginx_error.log exist, otherwise create the files.
 if [ -e logs/nginx_access.log ] && [ -e logs/nginx_error.log ]
 then
@@ -13,8 +17,12 @@ else
 fi
 
 # shellcheck disable=SC2232
-sudo cp -rf multi_apps_1_nginx.conf /etc/nginx/sites-available/multi_apps_1_nginx.conf
+sudo cp -rf $NGINX_CONF_FILE $NGINX_SITES_AVAILABLE_DIR/multi_apps_1_nginx.conf
+echo "Copied the '$NGINX_CONF_FILE' file in path: $NGINX_SITES_AVAILABLE_DIR"
 chmod 710 /var/lib/jenkins/workspace/multi_app_deploy_1
+
+sudo ln -s $NGINX_SITES_AVAILABLE_DIR/multi_apps_1_nginx.conf $NGINX_SITES_ENABLED_DIR
+echo "Created symlink of '$NGINX_SITES_AVAILABLE_DIR/multi_apps_1_nginx.conf' inside the path: $NGINX_SITES_ENABLED_DIR"
 
 sudo nginx -t
 
