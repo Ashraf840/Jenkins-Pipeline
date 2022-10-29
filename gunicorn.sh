cfc2e3e991bd065029aec2a5461d67b733d07852
ashraf840 <shift2unleashed2011@gmail.com>
2022-10-30 04:11:35 +0600

    Update(29) the 'Jenkinsfile' file

--- gunicorn.sh
+++ gunicorn.sh
@@ -19,7 +19,7 @@ echo "Django migrations have been done while executing gunicorn.sh!"
 echo "Present Directory: $PWD"
 
 # Make the gunicorn_server.sh executable
-sudo chmod u+x gunicorn_start.sh
+chmod u+x gunicorn_start.sh
 
 # Copy 'gunicorn_start.sh' file to the env/bin/ path of this django-project directory
 sudo cp -rf gunicorn_start.sh $DJANGODIR/env/bin/
