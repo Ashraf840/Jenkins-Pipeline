d875830004e6110ea3d9ee30b923c470ab6ee482
ashraf840 <shift2unleashed2011@gmail.com>
2022-10-29 22:36:52 +0600

    Update(5) the 'nginx.sh' file; user:nginx root-permission

--- nginx.sh
+++ nginx.sh
@@ -6,7 +6,6 @@ echo "Present Directory: $PWD"
 NGINX_CONF_FILE=multi_apps_1_nginx.conf
 NGINX_SITES_AVAILABLE_DIR=/etc/nginx/sites-available
 NGINX_SITES_ENABLED_DIR=/etc/nginx/sites-enabled
-USER=jenkins
 
 # Check if nginx_access.log & nginx_error.log exist, otherwise create the files.
 if [ -e logs/nginx_access.log ] && [ -e logs/nginx_error.log ]
@@ -24,8 +23,6 @@ chmod 710 /var/lib/jenkins/workspace/multi_app_deploy_1
 
 sudo ln -s $NGINX_SITES_AVAILABLE_DIR/multi_apps_1_nginx.conf $NGINX_SITES_ENABLED_DIR
 echo "Created symlink of '$NGINX_SITES_AVAILABLE_DIR/multi_apps_1_nginx.conf' inside the path: $NGINX_SITES_ENABLED_DIR"
-
-sudo chmod u+x /var/lib/jenkins/workspace/multi_app_deploy_1/run/gunicorn.sock
 
 sudo nginx -t
 
