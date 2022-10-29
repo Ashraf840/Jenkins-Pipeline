1936678308c98e5d116923e40cfca3316785eae9
ashraf840 <shift2unleashed2011@gmail.com>
2022-10-29 11:29:44 +0600

    Working(3) with 'nginx.sh' file

--- nginx.sh
+++ nginx.sh
@@ -3,5 +3,12 @@
 echo "running nginx.sh file"
 echo "Present Directory: $PWD"
 
-
+# Check if nginx_access.log & nginx_error.log exist, otherwise create the files.
+if [ -e logs/nginx_access.log ] && [ -e logs/nginx_error.log ]
+then
+    echo "'nginx_access.log' & 'nginx_error.log' files exist!"
+else
+    touch logs/nginx_access.log logs/nginx_error.log
+    echo "Created the 'nginx_access.log' & 'nginx_error.log' file!"
+fi
 
