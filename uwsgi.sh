c0fbe4b63595ce6c647ca20e15a60590e4e30a80
ashraf840 <shift2unleashed2011@gmail.com>
2022-10-30 16:57:01 +0600

    Checking vassalas dir exist, echos

--- uwsgi.sh
+++ uwsgi.sh
@@ -4,6 +4,13 @@ echo "running uwsgi.sh file!"
 echo "User: $USER"
 echo "Present Dir: $PWD"
 
+# Check if the 'vassals' dir exists, otherwise create the dir
+if [ -d /etc/uwsgi/vassals ]
+then
+    echo "vassals dir exists"
+else
+    echo "vassals dir doesn't exists"
+fi
 
 sudo systemctl status emperor.uwsgi.service
 
